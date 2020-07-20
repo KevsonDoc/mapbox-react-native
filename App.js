f983f7cbf13127c02b0f250f74d1e3c2d24a63c2
KevsonDoc <kevsonfilipesantos@gmail.com>
2020-07-19 23:36:51 -0300

    [add] - new template

--- App.js
+++ App.js
@@ -1,38 +1,24 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-import MapboxGL from "@react-native-mapbox-gl/maps";
+import { Text, StyleSheet, View, StatusBar } from "react-native";
 
-MapboxGL.setAccessToken("pk.eyJ1Ijoia2V2c29uZG9jIiwiYSI6ImNrY3Rwd2Z6YTIxMGwycnM1MHVrcnJ6aWwifQ.kb34aAVTeAVc4Ozr08sghA");
+import Maps from './src/maps';;
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <View style={styles.container}>
+        <Maps />
+      </View>
+    </>
+  );
+}
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
   container: {
-    height: 300,
-    width: 300,
-    backgroundColor: "tomato"
-  },
-  map: {
-    flex: 1
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "red"
   }
 });
 
-export default class App extends Component {
-  componentDidMount() {
-    MapboxGL.setTelemetryEnabled(false);
-  }
-
-  render() {
-    return (
-      <View style={styles.page}>
-        <View style={styles.container}>
-          <MapboxGL.MapView style={styles.map} />
-        </View>
-      </View>
-    );
-  }
-}
+export default App;
